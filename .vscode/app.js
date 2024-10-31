286e8a90eff1840a0fa9691dfb24559feee350fa
dfadsfdsfwee <124458500+dfadsfdsfwee@users.noreply.github.com>
2024-11-01 00:23:29 +0800

    emove lauch.js

---
+++ .vscode/app.js
@@ -0,0 +1,4 @@
+const SemManagement = require(".ChooYiZhen_SchoolManagement.js");  // CAN ALSO USE LET INSTEAD OF CONST
+
+console.log("Hello"); 
+
--- app.js
+++
@@ -1,6 +0,0 @@
-console.log("Starting my first Node JS application");
-const SemManagement = require("./ChooYiZhen_SchoolManagement.js");  // CAN ALSO USE LET INSTEAD OF CONST
-
-
-
-
